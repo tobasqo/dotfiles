9c4210239791416a93601f3b2a2a69b6dbed62b7
tobasqo <szymon.j.jaskowski@gmail.com>
2024-07-12 18:40:06 +0200

    fix bufferline

--- .config/nvim/lua/custom/plugins/bufferline.lua
+++ .config/nvim/lua/custom/plugins/bufferline.lua
@@ -1,15 +1,33 @@
 return {
-  'akinsho/bufferline.nvim',
-  event = 'VeryLazy',
+  "akinsho/bufferline.nvim",
+  event = "VeryLazy",
   keys = {
-    { '<leader>bo', '<Cmd>BufferLineCloseOthers<CR>', desc = 'Delete Other Buffers' },
-    { '<leader>br', '<Cmd>BufferLineCloseRight<CR>', desc = 'Delete Buffers to the Right' },
-    { '<leader>bl', '<Cmd>BufferLineCloseLeft<CR>', desc = 'Delete Buffers to the Left' },
-    { '<S-h>', '<cmd>BufferLineCyclePrev<cr>', desc = 'Prev Buffer' },
-    { '<S-l>', '<cmd>BufferLineCycleNext<cr>', desc = 'Next Buffer' },
-    { '[b', '<cmd>BufferLineCyclePrev<cr>', desc = 'Prev Buffer' },
-    { ']b', '<cmd>BufferLineCycleNext<cr>', desc = 'Next Buffer' },
-    { '[B', '<cmd>BufferLineMovePrev<cr>', desc = 'Move buffer prev' },
-    { ']B', '<cmd>BufferLineMoveNext<cr>', desc = 'Move buffer next' },
+    { "<leader>br", "<Cmd>BufferLineCloseRight<CR>", desc = "Delete Buffers to the Right" },
+    { "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>", desc = "Delete Buffers to the Left" },
+    { "<S-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
+    { "<S-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
+    { "<leader>bp", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
+    { "<leader>bn", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
+    { "[B", "<cmd>BufferLineMovePrev<cr>", desc = "Move buffer prev" },
+    { "]B", "<cmd>BufferLineMoveNext<cr>", desc = "Move buffer next" },
   },
+  config = function()
+    require("bufferline").setup({
+      options = {
+        diagnostics = "nvim_lsp",
+        offsets = {
+          {
+            filetype = "neo-tree",
+            text = "File Explorer",
+            text_align = "left",
+          },
+        },
+        show_buffer_icons = true,
+        show_buffer_close_icons = true,
+        show_close_icon = true,
+        separator_style = "thin",
+        always_show_bufferline = true,
+      },
+    })
+  end,
 }
