9571fa09e0ba1372a038c637fd45d26838ef87ba
tobasqo <szymon.j.jaskowski@gmail.com>
2024-07-17 20:26:44 +0200

    plugins update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,5 @@
+vim.opt.termguicolors = true
+
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
@@ -202,21 +204,22 @@ require("lazy").setup({
     "folke/which-key.nvim",
     event = "VimEnter", -- Sets the loading event to 'VimEnter'
     config = function() -- This is the function that runs, AFTER loading
-      require("which-key").setup()
+      -- require("which-key").setup()
 
       -- Document existing key chains
-      require("which-key").register({
-        ["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
-        ["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
-        ["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
-        ["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
-        ["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
-        ["<leader>t"] = { name = "[T]oggle", _ = "which_key_ignore" },
-        ["<leader>h"] = { name = "Git [H]unk", _ = "which_key_ignore" },
+      local wk = require("which-key")
+      wk.add({
+        { "<leader>c", desc = "[C]ode" },
+        { "<leader>d", desc = "[D]ocument" },
+        { "<leader>r", desc = "[R]ename" },
+        { "<leader>s", desc = "[S]earch" },
+        { "<leader>w", desc = "[W]orkspace" },
+        { "<leader>t", desc = "[T]oggle" },
+        { "<leader>h", desc = "Git [H]unk" },
       })
       -- visual mode
-      require("which-key").register({
-        ["<leader>h"] = { "Git [H]unk" },
+      wk.add({
+        { "<leader>h", desc = "Git [H]unk" },
       }, { mode = "v" })
     end,
   },
@@ -723,9 +726,7 @@ require("lazy").setup({
       vim.cmd.hi("Comment gui=none")
     end,
     opts = {
-      on_highlights = function(hl, _)
-        local colors = require("tokyonight.colors")
-        local c = colors.default
+      on_highlights = function(hl, c)
         hl["@constructor"] = { fg = c.blue1 }
         hl["Include"] = { fg = c.purple }
         hl["@constant.builtin"] = { fg = c.cyan }
--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -4,40 +4,40 @@
 -- See the kickstart.nvim README for more information
 return {
   {
-    'norcalli/nvim-colorizer.lua',
+    "norcalli/nvim-colorizer.lua",
     config = function()
-      require('colorizer').setup()
+      require("colorizer").setup()
     end,
   },
 
   {
-    'christoomey/vim-tmux-navigator',
-    vim.keymap.set('n', '<c-h>', ':TmuxNavigateLeft<cr>'),
-    vim.keymap.set('n', '<C-j>', ':TmuxNavigateDown<CR>'),
-    vim.keymap.set('n', '<C-k>', ':TmuxNavigateUp<CR>'),
-    vim.keymap.set('n', '<C-l>', ':TmuxNavigateRight<CR>'),
+    "christoomey/vim-tmux-navigator",
+    vim.keymap.set("n", "<c-h>", ":TmuxNavigateLeft<cr>"),
+    vim.keymap.set("n", "<C-j>", ":TmuxNavigateDown<CR>"),
+    vim.keymap.set("n", "<C-k>", ":TmuxNavigateUp<CR>"),
+    vim.keymap.set("n", "<C-l>", ":TmuxNavigateRight<CR>"),
   },
 
-  { 'windwp/nvim-ts-autotag' },
+  { "windwp/nvim-ts-autotag" },
 
-  { 'echasnovski/mini.move' },
+  { "echasnovski/mini.move" },
 
-  { 'nvim-treesitter/nvim-treesitter-context' },
+  { "nvim-treesitter/nvim-treesitter-context" },
 
   {
-    'kdheepak/lazygit.nvim',
+    "kdheepak/lazygit.nvim",
     cmd = {
-      'LazyGit',
-      'LazyGitConfig',
-      'LazyGitCurrentFile',
-      'LazyGitFilter',
-      'LazyGitFilterCurrentFile',
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
     },
     dependencies = {
-      'nvim-lua/plenary.nvim',
+      "nvim-lua/plenary.nvim",
     },
     keys = {
-      { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
+      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
     },
   },
 }
