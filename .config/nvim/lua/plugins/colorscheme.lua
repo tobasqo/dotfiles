76539a99ba2c82be7b512a3d9f310635cc94e9a4
tobasqo <szymon.j.jaskowski@gmail.com>
2026-04-08 16:53:10 +0200

    adjust colors for python

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -86,10 +86,12 @@ return {
         groups = {
           github_dark_dimmed = {
             ["@punctuation.delimiter"] = { fg = "#77bdfb" },
+            ["@attribute.builtin.python"] = { fg = "#77bdfb" },
             ["@parameter"] = { fg = "red" },
             ["@module.python"] = { fg = "#adbac7" },
             ["@variable.builtin.python"] = { fg = "#a2d2fb", style = "italic" },
             ["@keyword.exception.python"] = { fg = "#f47067" },
+            ["@keyword.coroutine.python"] = { fg = "#f47067", style = "italic" },
           },
         },
       })
